a6bb151030f65da8e6cb5622d0d7bd2bdf790501
katiecray <k.cray0120201@arts.ac.uk>
2021-04-26 09:17:05 +0100

    Few changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let bot = new RiveScript();
 
-const brains = ["brain/subs.rive", "brain/brain.rive", "brain/katie.rive"];
+const brains = ["brain/subs.rive", "brain/brain.rive", "brain/katie.rive", "brain/Naiwen.rive"];
 
 bot.loadFile(brains).then(botReady).catch(botNotReady);
 
